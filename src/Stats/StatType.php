3939780d5730334c806d497d8074aee729e6f468
Joso997 <jspmari@gmail.com>
2023-11-21 22:22:49 +0100

    fix

--- src/Stats/StatType.php
+++ src/Stats/StatType.php
@@ -12,6 +12,22 @@ class StatType
     public static function getCaseFunctionMapping (): array {
         return [
             StatsEnum::Label->value => new StatClosure(fn(): StatAbstract => (new Label())->createStat()),
+            StatsEnum::Value->value => new StatClosure(fn() => (new Value())->createStat()),
+            StatsEnum::Design->value => new StatClosure(fn() => (new Design())->createStat()),
+            StatsEnum::Tag->value => new StatClosure(fn() => (new Tag())->createStat()),
+            StatsEnum::Id->value => new StatClosure(fn() => (new Id())->createStat()),
+            StatsEnum::ElementType->value => new StatClosure(fn() => (new ElementType())->createStat()),
+            StatsEnum::Placeholder->value => new StatClosure(fn() => (new Placeholder())->createStat()),
+            StatsEnum::ItemList->value => new StatClosure(fn() => (new ItemList())->createStat()),
+            StatsEnum::Tooltip->value => new StatClosure(fn() => (new Tooltip())->createStat()),
+            StatsEnum::Required->value => new StatClosure(fn() => (new Required())->createStat()),
+            StatsEnum::Disabled->value => new StatClosure(fn() => (new Disabled())->createStat()),
+            StatsEnum::AutoComplete->value => new StatClosure(fn() => (new AutoComplete())->createStat()),
+            StatsEnum::BelongsTo->value => new StatClosure(fn() => (new BelongsTo())->createStat()),
+            StatsEnum::ErrorMessage->value => new StatClosure(fn() => (new ErrorMessage())->createStat()),
+            StatsEnum::IsValid->value => new StatClosure(fn() => (new IsValid())->createStat()),
+            StatsEnum::Order->value => new StatClosure(fn() => (new Order())->createStat()),
+            StatsEnum::DependsOn->value => new StatClosure(fn() => (new DependsOn())->createStat()),
         ];
     }
 }
