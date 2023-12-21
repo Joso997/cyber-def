ed60f6f9b49edf899b58fd629abbe2c2140d4209
DESKTOP-TOJLL8R\10Administrator <Josip.Maric@tri-m.hr>
2023-12-21 15:30:39 +0100

    fix

--- src/FormComponents/InputGroupComponent.php
+++ src/FormComponents/InputGroupComponent.php
@@ -7,15 +7,20 @@ use Cybertale\Definition\Helpers\ActionsEnum;
 use Cybertale\Definition\Helpers\RegionsEnum;
 use Cybertale\Definition\Helpers\StatsEnum;
 use Cybertale\Definition\ObjectTemplate;
+use Cybertale\Definition\Stats\StatType;
 
 class InputGroupComponent extends ComponentAbstract
 {
     public function __construct(ObjectTemplate ...$objectTemplates)
     {
         $arr = [];
+        $inherits = [StatsEnum::Inherit->value =>
+            StatType::getCaseFunctionMapping()[StatsEnum::Inherit->value]
+                ->setData(json_encode([StatsEnum::Value->value, StatsEnum::ErrorMessage->value, StatsEnum::IsValid->value]))
+        ];
         foreach ($objectTemplates as $objectTemplate) {
             $objectTemplate->setRegion(RegionsEnum::Form)
-                ->setStats([StatsEnum::Inherit->value => json_encode([StatsEnum::Value->value, StatsEnum::ErrorMessage->value, StatsEnum::IsValid->value])]);
+                ->setStats(array_merge($objectTemplate->getStats(),$inherits));
             $arr[] = $objectTemplate;
         }
         $objectTemplates = $arr;
