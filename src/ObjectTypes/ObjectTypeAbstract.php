3a9f286b238c56590d640ecfe7e5676c91ec9876
TRI-M\jmaric <josip.maric@tri-m.hr>
2025-11-20 17:09:11 +0100

    maybe

--- src/ObjectTypes/ObjectTypeAbstract.php
+++ src/ObjectTypes/ObjectTypeAbstract.php
@@ -9,31 +9,35 @@ use Cybertale\Definition\Stats\StatType;
 
 abstract class ObjectTypeAbstract
 {
-    /**
-     * @var array<int, StatAbstract>
-     */
-    protected array $stats;
+    protected array $stats = [];
     public abstract function get(): ObjectTemplate;
 
     public function setStats(StatsEnum $statsEnum, string|bool|null $data): ObjectTypeAbstract
     {
-        if (!isset($this->stats[$statsEnum->value])) {
-            $this->stats[$statsEnum->value] = StatType::getCaseFunctionMapping()[$statsEnum->value]->createStat();
+        $newStat = StatType::getCaseFunctionMapping()[$statsEnum->value]->setData($data);
+
+        if (isset($this->stats[$statsEnum->value])) {
+            // copy existing metadata into new object
+            $newStat->setMetaData($this->stats[$statsEnum->value]->getMetaData());
         }
 
-        $this->stats[$statsEnum->value]->setData($data);
+        $this->stats[$statsEnum->value] = $newStat;
+
         return $this;
     }
 
-    public function setStatsMeta(StatsEnum $statsEnum, string|bool|null $data): ObjectTypeAbstract
+    public function setStatsMeta(StatsEnum $statsEnum, string|bool|null $meta): ObjectTypeAbstract
     {
-        if (!isset($this->stats[$statsEnum->value])) {
-            $this->stats[$statsEnum->value] = StatType::getCaseFunctionMapping()[$statsEnum->value]->createStat();
+        $newStat = StatType::getCaseFunctionMapping()[$statsEnum->value]->setMetaData($meta);
+
+        if (isset($this->stats[$statsEnum->value])) {
+            // copy existing data into new object
+            $newStat->setData($this->stats[$statsEnum->value]->getData());
         }
 
-        $this->stats[$statsEnum->value]->setMetaData($data);
+        $this->stats[$statsEnum->value] = $newStat;
+
         return $this;
     }
-
-
 }
+
